c3b222d7b736d187a72d04332e795a1604ca1e43
AkarinLiu <akarinliu@outlook.com>
2024-03-25 16:24:46 +0800

    清空/var/www/html目录下的文件

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ if [ ! -f /var/www/html/site.php ]; then
     exit 0
 else
     echo "Detect Not Installed, Installing..."
-    rm -rf /var/www/html
+    rm -rf /var/www/html/*
     composer create-project flarum/flarum /var/www/html
     chown -R www-data:www-data /var/www/html
     chmod -R 755 /var/www/html
